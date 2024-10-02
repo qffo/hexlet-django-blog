f5d15dc56cf60acfa37854120f50eb9de2db648b
qffo <075viktor@mail.ru>
2024-10-02 16:01:48 +0300

    feat(article): add new project article

--- hexlet_django_blog/urls.py
+++ hexlet_django_blog/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from hexlet_django_blog import views
 
 urlpatterns = [
     path('', views.index),
-    path('about/', views.about),  # <- добавляем эту строчку
+    path('about/', views.about),
+    path('articles/', include('hexlet_django_blog.article.urls')),
     path('admin/', admin.site.urls),
 ]
