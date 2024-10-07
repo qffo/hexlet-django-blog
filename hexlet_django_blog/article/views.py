757762ec10c7fdea002f0aefcb5d3f23e0d7e101
qffo <075viktor@mail.ru>
2024-10-07 11:53:28 +0300

    add black theme

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,11 +1,3 @@
-from django.shortcuts import render
-
-
-# def index(request):
-#     return render(request, 'article/index.html', context={
-#         'who': 'article',
-#     })
-
 from hexlet_django_blog.article.models import Article
 from django.views import View
 from django.shortcuts import render
--- hexlet_django_blog/urls.py
+++ hexlet_django_blog/urls.py
@@ -5,10 +5,6 @@ from .views import HomePageView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='index'),
-    # path('', views.index),
-    # path('', views.IndexView.as_view()),
-    # path('', TemplateView.as_view(template_name='index.html')),
-    # path('', IndexView.as_view(), name='index'),
     path('about/', views.about),
     path('articles/', include('hexlet_django_blog.article.urls')),
     path('admin/', admin.site.urls),
